269eb5d6d6672a4dd5b0a9d4746cc9dab039e606
shivamdatascience <sbixbxn@gmail.com>
2021-05-03 13:19:25 +0530

    Updates

---
+++ precache-manifest.8bca1607cc2e968d594497e6413866cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "99f65f257adecdae836e",
+    "url": "/portfolio100/static/css/main.c079f164.chunk.css"
+  },
+  {
+    "revision": "99f65f257adecdae836e",
+    "url": "/portfolio100/static/js/main.99f65f25.chunk.js"
+  },
+  {
+    "revision": "455d3a4716702d5cd61c",
+    "url": "/portfolio100/static/js/1.455d3a47.chunk.js"
+  },
+  {
+    "revision": "53cd4fc8890f2dfd4501",
+    "url": "/portfolio100/static/js/runtime~main.53cd4fc8.js"
+  },
+  {
+    "revision": "bbec13af0b49034872cce84412e44596",
+    "url": "/portfolio100/index.html"
+  }
+];
